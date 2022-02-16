ba34175445a69cd50a67ee1517b2854943f7f8f0
Fernando Salcedo <fsalcedo55@hotmail.com>
2022-02-16 00:02:13 -0500

    Finish top half of page

---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+import React from "react";
+import photogrid from "../images/photo-grid.png";
+
+export default function Hero() {
+  return (
+    <div className="hero-div">
+      <img
+        src={photogrid}
+        alt="airbnb-front-page-grid"
+        className="photo-grid"
+      />
+      <h1>Online Experiences</h1>
+      <p>
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </div>
+  );
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import logo from "../images/airbnb-logo.png";
 
 export default function Navbar() {
-    return (
-        <img src="" />
-    )
+  return (
+    <div className="navbar">
+      <img src={logo} alt="airbnb-logo" className="airbnb-logo" />
+    </div>
+  );
 }
