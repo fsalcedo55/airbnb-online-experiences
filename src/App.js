5c403cb7fffa4de80485d2e07d4acbbed0c54b22
Fernando Salcedo <fsalcedo55@hotmail.com>
2022-02-17 13:12:32 -0500

    Pass props to card component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-// import Hero from "./components/Hero";
+import Hero from "./components/Hero";
 import Card from "./components/Card";
+import photo from "./images/swimmer.jpeg";
 import "./styles.css";
 
 export default function App() {
+  const cardInfo = {
+    img: "../images/swimmer.jpeg",
+    rating: 5.0,
+    reviewCount: 6,
+    country: "Slovenia",
+    title: "Life Lessons with Michael Phelps",
+    price: 136,
+  };
+
   return (
     <div>
       <Navbar />
-      {/* <Hero /> */}
+      <Hero />
       <div>
-        <Card />
+        <Card
+          img={photo}
+          rating={cardInfo.rating}
+          reviewCount={cardInfo.reviewCount}
+          country={cardInfo.country}
+          title={cardInfo.title}
+          price={cardInfo.price}
+        />
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,23 @@
 import React from "react";
-import photo from "../images/swimmer.jpeg";
+// import photo from "../images/swimmer.jpeg";
 import redstar from "../images/redstar.png";
 
 export default function Card(props) {
   return (
     <div className="card--container">
-      <img src={photo} alt="swimmer" className="card--photo" />
+      <img src={props.img} alt="swimmer" className="card--photo" />
       <div>
         <div className="card--text">
           <div className="card--rating">
             <img src={redstar} alt="redstar" className="card--red-star" />
-            <p>5.0</p>
-            <p>(6)</p>
+            <p>{props.rating}</p>
+            <p>({props.reviewCount})</p>
             <p> • </p>
-            <p>USA</p>
+            <p>{props.country}</p>
           </div>
-          <p>Life lessons with a Michael Phelps</p>
+          <p>{props.title}</p>
           <p>
-            <b>From $136</b> / person
+            <b>${props.price}</b> / person
           </p>
         </div>
       </div>
