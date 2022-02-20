3ca713c88d98c8df6871559ca01da1c28d8c5dd4
Fernando Salcedo <fsalcedo55@hotmail.com>
2022-02-19 19:43:14 -0500

    Add screenshot

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export default [
   {
     id: 1,
-    title: "Life Lessons with Katie Zaferes",
+    title: "Life Lessons with a Swimmer",
     description:
       'I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.',
     price: 136,
